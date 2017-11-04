30c6527997f3a4352792185025eeb7e7ee9f4cdc
JLIVE101 <jwilliams199055@gmail.com>
2017-11-04 10:45:26 -0400

    added route to save member and config json file

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,33 @@
 var express = require('express');
 var Member = require('../models/member');
+var config = require('../config.json');
 var memberRouter = express.Router();
+
+var googleMapsClient = require('@google/maps').createClient({
+    key: config.GOOGLEMAPS_API_KEY
+});
+
+
+memberRouter.post('/save', function (req, res, next) {
+
+    var body = req.body;
+
+    //validate member object
+
+    //triangulate location to set flag// Geocode an address.
+    googleMapsClient.geocode({
+        address: 'The 519 Catchment Area'
+    }, function (err, response) {
+        if (!err) {
+            res.send(response);
+        }
+    });
+
+
+    //save the user
+
+
+
+});
 
 module.exports = memberRouter;
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var morgan = require('morgan')
 var bodyParser = require('body-parser');
 var memberRouter = require('./routes/member');
+var config = require('./config.json');
 
 var app = express();
 var port = 8080;
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+//member routes
 app.use('/api', memberRouter);
+
 
 app.listen(port, () => {
   console.log('gtc17 running on port ' + port);
