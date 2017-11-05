beee7fd2e9b962c288ea58128f35905dbcfe3d0a
dburandt <behringer.19@gmail.com>
2017-11-05 11:27:51 -0500

    added logstash

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var morgan = require('morgan')
+var morgan = require('morgan');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 var memberRouter = require('./routes/member');
 var config = require('./config/config.json');
@@ -7,12 +8,13 @@ var config = require('./config/config.json');
 var app = express();
 var port = 8080;
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 //member routes
 app.use('/api', memberRouter);
-
+app.use('/api', taskRouter);
 
 app.listen(port, () => {
   console.log('gtc17 running on port ' + port);
